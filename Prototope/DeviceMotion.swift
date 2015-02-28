dfe8fad3a6d206ac7cc7f2c608ea0e193bf73ff1
Saniul Ahmed <saniul.ahmed@gmail.com>
2015-02-27 19:36:59 -0800

    Expose a polling interface for device attitude samples
    The last 10 samples are exposed (internally stored in a ring buffer).
    When initializing the AttitudeObserver you can decide whether you want
    the samples to be recorder relative to an anchor attitude or just as absolute
    values.

--- Prototope/DeviceMotion.swift
+++ Prototope/DeviceMotion.swift
@@ -10,11 +10,14 @@ import Foundation
 import CoreMotion
 
 public struct DeviceAttitudeSample {
-    let roll: Double
-    let pitch: Double
-    let yaw: Double
+    public let timestamp: TimeInterval
     
-    init(cmAttitude: CMAttitude) {
+    public let roll: Double
+    public let pitch: Double
+    public let yaw: Double
+    
+    init(timestamp: TimeInterval, cmAttitude: CMAttitude) {
+        self.timestamp = timestamp
         self.roll = cmAttitude.roll
         self.pitch = cmAttitude.pitch
         self.yaw = cmAttitude.yaw
@@ -24,13 +27,51 @@ public struct DeviceAttitudeSample {
 public class DeviceAttitudeObserver {
     private let motionManager: CMMotionManager = CMMotionManager()
     
-    typealias DeviceAttitudeHandler = DeviceAttitudeSample -> Void
+    private var anchor: CMAttitude!
     
-    private let handler: DeviceAttitudeHandler
+    private var sampleBuffer: Ring<DeviceAttitudeSample>
     
-    func deviceMotionHandler(deviceMotion: CMDeviceMotion) {
-        let sample = DeviceAttitudeSample(cmAttitude: deviceMotion.attitude)
-        handler(sample)
+    public var latestSamples: [DeviceAttitudeSample] { return reverse(lazy(sampleBuffer).array) }
+    
+    private let relativeToAnchor: Bool
+    
+    public init(relativeToAnchor: Bool = true) {
+        self.sampleBuffer = Ring(capacity: 10)
+        self.relativeToAnchor = relativeToAnchor
+        
+        motionManager.startDeviceMotionUpdatesUsingReferenceFrame(CMAttitudeReferenceFrame.XArbitraryCorrectedZVertical,
+            toQueue: NSOperationQueue.mainQueue()) { [weak self] (deviceMotion, error) -> Void in
+                if error != nil {
+                    self?.errorHandler(error)
+                }
+                
+                if deviceMotion != nil {
+                    self?.updateWithDeviceMotion(deviceMotion)
+                }
+        }
+    }
+    
+    public func resetAnchor() {
+        self.anchor = nil
+    }
+    
+    func updateAnchor() {
+        self.anchor = self.motionManager.deviceMotion.attitude
+    }
+    
+    func updateWithDeviceMotion(deviceMotion: CMDeviceMotion) {
+        
+        if anchor == nil {
+            updateAnchor()
+        }
+        
+        let translated = deviceMotion.attitude
+        if relativeToAnchor {
+            translated.multiplyByInverseOfAttitude(anchor)
+        }
+        
+        let sample = DeviceAttitudeSample(timestamp: deviceMotion.timestamp, cmAttitude: translated)
+        sampleBuffer.add(sample)
     }
     
     func errorHandler(error: NSError) {
@@ -38,18 +79,66 @@ public class DeviceAttitudeObserver {
             motionManager.showsDeviceMovementDisplay = true
         }
     }
+}
+
+public struct RingGenerator<T> : GeneratorType {
+    private let buffer: [T]
+    private var idx: Int
+    private var remainingCount: Int
     
-    init(handler: DeviceAttitudeHandler) {
-        self.handler = handler
-        
-        motionManager.startDeviceMotionUpdatesUsingReferenceFrame(CMAttitudeReferenceFrame.XArbitraryCorrectedZVertical, toQueue: NSOperationQueue.mainQueue()) { (deviceMotion, error) -> Void in
-            if error != nil {
-                self.errorHandler(error)
-            }
-            
-            if deviceMotion != nil {
-                self.deviceMotionHandler(deviceMotion)
-            }
+    public mutating func next() -> T? {
+        if remainingCount > 0 {
+            let value = buffer[idx % buffer.count]
+            remainingCount--
+            idx++
+            return value
         }
+        return nil
+    }
+    
+    private init(_ ring: Ring<T>) {
+        buffer = ring.buffer
+        remainingCount = ring.storedCount
+        idx = (ring.count-remainingCount) % ring.capacity
+    }
+}
+
+
+public struct Ring<T> : SequenceType {
+    
+    private var buffer = [T]()
+    
+    public private(set) var count = 0
+    
+    public let capacity: Int
+    
+    public var storedCount: Int {
+        return min(count, capacity)
+    }
+    
+    public init(capacity: Int) {
+        assert(capacity > 0)
+        self.capacity = capacity
+    }
+    
+    mutating public func add(value: T) {
+        if self.isFull {
+            buffer[count % buffer.count] = value
+        } else {
+            buffer.append(value)
+        }
+        ++count
+    }
+    
+    public var isEmpty: Bool {
+        return count == 0
+    }
+    
+    public var isFull: Bool {
+        return count >= capacity
+    }
+    
+    public func generate() -> RingGenerator<T> {
+        return RingGenerator(self)
     }
 }
